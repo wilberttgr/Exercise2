a0622fb8dcf94f1cbf2fca9d0e4811b58769a199
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-11-03 16:02:30 +0700

    add untuk menentukan banyak data

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -12,6 +12,10 @@ namespace Exercise2
         {
             int i, n, wt, temp;
             int[] arr = new int[50];
+
+            // tent banyak data
+            Console.Write("Jumlah Data : ");
+            n = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
